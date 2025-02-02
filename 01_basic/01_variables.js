6c7f2bd540f6b87898eafc29e2091cfab97ceab6
ayushvan kumar <73921204+ayushwan@users.noreply.github.com>
2025-02-02 18:26:08 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accoutId = 144553 
+let accoutEmail = "akppji@gmail.com"
+var accountPassword = "23434"
+accoutCity = "Ranchi"
+let accountState;
+
+// accoutId = 2
+// console.log(accoutId); 
+
+/* prefer not to write var because there is a issue with this */
+
+accoutEmail = "hc@gmial.com"
+accountPassword = "e43353"
+accoutCity = "Daltonganj"
+
+console.log(accoutId);
+
+console.table([accoutId, accoutEmail, accountPassword, accoutCity, accountState]);
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3)  //we are using nodejs, not browser
+
+console.log(3+3);
+console.log("Ayushwan");
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined 
+// Symbol => unique
+
+
+// Object 
+
+
+console.log(typeof "Aysuh"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
