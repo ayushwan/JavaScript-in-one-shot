e1141704ff8f0c3f34fd5f3b0686eaf53f530cee
ayushvan kumar <73921204+ayushwan@users.noreply.github.com>
2025-02-12 19:04:11 +0000

    math and num

---
+++ 01_basic/07_nums_and_math.js
@@ -0,0 +1,38 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const otherNum = 23.2343;
+// console.log(otherNum.toPrecision(5));
+
+// const num2 = 1000000;
+// console.log(num2.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++ Math ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.max(23, 31, 23, 53, 54, 75));
+// console.log(Math.min(23, 31, 23, 53, 54, 75));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 1
+const max = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
